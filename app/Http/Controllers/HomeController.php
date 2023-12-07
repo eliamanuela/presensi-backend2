93d6a8871c11adb7748834ae27ca340efa71f75b
Hotman <projecthotman@gmail.com>
2023-12-07 08:32:30 +0700

    sudah

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,9 +29,9 @@ class HomeController extends Controller
     public function index()
     {
         $presensis = Presensi::select('presensis.*', 'users.name')
-                        ->join('users', 'presensis.user_id', '=', 'users.id')
-                        ->get();
-        foreach($presensis as $item) {
+            ->join('users', 'presensis.user_id', '=', 'users.id')
+            ->get();
+        foreach ($presensis as $item) {
             $datetime = Carbon::parse($item->tanggal)->locale('id');
 
             $datetime->settings(['formatFunction' => 'translatedFormat']);
@@ -39,31 +39,29 @@ class HomeController extends Controller
             $item->tanggal = $datetime->format('l, j F Y');
         }
         $kehadiran = Presence::query()
-        ->select('table_present.presence', 'users.name', 'table_present.bulan_karyawan')
-        ->join('users', 'table_present.user_id', '=', 'users.id')
-        ->paginate(5);
+            ->select('table_present.presence', 'users.name', 'table_present.bulan_karyawan')
+            ->join('users', 'table_present.user_id', '=', 'users.id')
+            ->paginate(5);
 
         $users = User::query()
-        ->select('users.*')
-        ->selectSub(function ($query) {
-            $query->selectRaw('COUNT(*)')
-            ->from('presensis')
-            ->whereColumn('users.id', 'presensis.user_id')
-            ->groupBy('presensis.user_id');
-        }, 'total_presensi')
-        ->where('role', 'user')
-        ->leftJoin('presensis', 'users.id', '=', 'presensis.user_id')
-        ->paginate(5);
+            ->select('users.*')
+            ->selectSub(function ($query) {
+                $query->selectRaw('COUNT(*)')
+                    ->from('presensis')
+                    ->whereColumn('users.id', 'presensis.user_id')
+                    ->groupBy('presensis.user_id');
+            }, 'total_presensi')
+            ->where('role', 'user')
+            ->leftJoin('presensis', 'users.id', '=', 'presensis.user_id')
+            ->distinct('users.id')
+            ->paginate(5);
 
         $user = User::query()
-        ->select('users.name', 'users.id')
-        ->where('role', 'user')
-        ->get();
-        // $bulan = bulanModel::query()
-        //     ->select('bulan.id','bulan.nama_bulan')
-        //     ->get();
+            ->select('users.name', 'users.id')
+            ->where('role', 'user')
+            ->get();
 
-        return view('home',compact([
+        return view('home', compact([
             'presensis',
             'users',
             'user',
